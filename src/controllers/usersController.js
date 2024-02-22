5bbb5e685f8e4e731bee35e0b5e338e5246b9a20
Lennon Fonseca <Lennonfonsecaa@gmail.com>
2024-02-22 14:21:36 -0300

    create appError

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,7 +1,11 @@
-
+const appError = require('../utils/appError')
 class UsersController {
      create(request, response) {
         const { name, email, password, avatar } = request.body
+
+        if(!name) {
+            throw new appError('nome é obrigatório!')
+        }
 
         response.json({ name, email, password, avatar })
     }
--- src/server.js
+++ src/server.js
@@ -1,11 +1,26 @@
 //server devolve p cliente. e devolve oq vem do 'app.use(routes)'
-
+require('express-async-errors')
+const appError = require('./utils/appError')
 const express = require('express')
 const routes = require('./routes') //importando routes. quando não passar a pasta que quer utilizar, por padrão vai encontrar o index.js
 
 const app = express() //executando express
 app.use(express.json())
 app.use(routes) //executando routes
+
+app.use(( error, request, response, next) => {
+    if(error instanceof appError) { //se o erro vem do cliente
+        return response.status(error.statusCode).json({
+            status: 'error',
+            message: error.message
+        })
+    }
+
+    return response.status(500).json({ //erro do servidor
+        status: 'error',
+        message: 'Internal server error'
+    })
+})
 
 
 const PORT = 3000
