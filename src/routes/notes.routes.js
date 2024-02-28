d122d73f500fa7f79a839f2058e1c653ea89e8f4
Lennon Fonseca <Lennonfonsecaa@gmail.com>
2024-02-28 18:51:24 -0300

    create index

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -7,6 +7,7 @@ const notesController = new NotesController()
 
 
 
+notesRoutes.get('/', notesController.index) //está passando sem parâmetro porque será passado o parâmetro dentro do insomnia
 notesRoutes.post('/:user_id', notesController.create) //userController vai criar o conteúdo e esse usersRoutes vai ser passado no index.
 notesRoutes.get('/:id', notesController.show) //userController vai criar o conteúdo e esse usersRoutes vai ser passado no index.
 notesRoutes.delete('/:id', notesController.delete)
