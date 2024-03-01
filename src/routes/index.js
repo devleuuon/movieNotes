16bad9f67dce6a5fdf5d260b664bca84649040e3
Lennon Fonseca <Lennonfonsecaa@gmail.com>
2024-03-01 11:40:47 -0300

    tagsController created

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@
 const { Router } = require('express') //importando Router do express
 const usersRoutes = require('./users.routes') //importando do user.routes
 const notesRoutes = require('./notes.routes')
+const tagsRoutes = require('./tags.routes')
 
 const routes = Router() //rodando o Router
 
 routes.use('/users', usersRoutes) //toda vez que for acessar '/user' vai ser redirecionado para usersRouter.
 routes.use('/movie_notes', notesRoutes)
+routes.use('/movie_tags', tagsRoutes)
 
 module.exports = routes
